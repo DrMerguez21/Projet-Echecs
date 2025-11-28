d2702886a81c28d5f368a93d05b6df46f043cc6f
DrMerguez21 <pltguest@ad.polytech.sorbonne-universite.fr@pc4007-linux.ad.polytech.sorbonne-universite.fr>
2025-11-28 15:21:17 +0100

    Création de Structures

---
+++ Structures.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+enum nom
+{
+    Pion, Tour, Cavalier, Fou, Reine, Roi
+};
+typedef enum nom NomPiece ;
+
+enum couleur
+{
+    Blanc, Noir
+};
+typedef enum couleur Couleur ;
+
+struct piece {
+    NomPiece name ;
+    Couleur C ;
+    int Point ;
+    int CptMvt ;
+    char Symbole ;
+} ;
+typedef struct piece Piece
+
+
+
+
