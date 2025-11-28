e57d477977c1a11036470d87639adafa12743fe8
DrMerguez21 <pltguest@ad.polytech.sorbonne-universite.fr@pc4007-linux.ad.polytech.sorbonne-universite.fr>
2025-11-28 15:55:23 +0100

    Fichier H

---
+++ Structures.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+enum nom
+{
+    Pion, Tour, Cavalier, Fou, Reine, Roi
+};
+typedef enum nom NomPiece ;
+
+enum couleur
+{
+    Blanc, Noir
+};
+typedef enum couleur Couleur ;
+
+struct piece {
+    NomPiece name ;
+    Couleur C ;
+    int Point ;
+    int CptMvt ;
+    char Symbole ;
+} ;
+typedef struct piece Piece
+
+// Tableau de pointeurs sur pièces
+// Faire les coups
