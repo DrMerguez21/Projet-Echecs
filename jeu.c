19bedd2c7a98f3bd7892c63286a6ad5ad9f436d7
SoulK3eper <Youkie5222@free.fr>
2025-12-12 16:27:51 +0100

    prise en compte des conversions de case

--- jeu.c
+++ jeu.c
@@ -7,28 +7,47 @@
 int main(){
     //ajouter ici la recharge de sauvegarde
     Piece*** plateau=ini_plateau();
-    printf("les pièces noires sont écrites en haut, les blanches en bas (les couleurs semblent inversées si on est en affichage sombre), les x correspondent aux lignes et les y aux colonnes\n");
+    printf("les pièces noires sont en haut, les blanches en bas (les couleurs semblent inversées si on est en affichage sombre), les x correspondent aux lignes et les y aux colonnes\n");
 
     while(!fini(Piece*** plateau)){ //fonction fini pas encore faite, renvoie 1 si un roi a été dévoré
+        char yd, yf;
+        int xd, xf;
         int xdebut, ydebut, xfin, yfin;
+
         affiche_plateau(plateau);
         printf("Au tour des blancs\n");
         //debut du décompte pour le joueur blanc
-        printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
-        scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+        printf("Entrez les coordonnées de la pièce que vous voulez déplacer, puis celles de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+        scanf("%d %d %d %d",&xd, &yd, &xf, &yf);
+        xdebut=convert_x(xd);
+        ydebut=convert_y(yd);
+        xfin=convert_x(xf);
+        yfin=convert_y(yf);
         while(!mouvement(plateau, xdebut, ydebut, xfin, yfin)){
-            printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
-            scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+            printf("Entrez les coordonnées de la pièce que vous voulez déplacer, puis celles de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+            scanf("%d %d %d %d",&xd, &yd, &xf, &yf);
+            xdebut=convert_x(xd);
+            ydebut=convert_y(yd);
+            xfin=convert_x(xf);
+            yfin=convert_y(yf);
         }
         //fin du décompte pour le joueur blanc
         affiche_plateau(plateau);
         printf("Au tour des noirs\n");
         //debut du décompte pour le joueur noir
-        printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
-        scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+        printf("Entrez les coordonnées de la pièce que vous voulez déplacer, puis celles de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+        scanf("%d %d %d %d",&xd, &yd, &xf, &yf);
+        xdebut=convert_x(xd);
+        ydebut=convert_y(yd);
+        xfin=convert_x(xf);
+        yfin=convert_y(yf);
         while(!mouvement(plateau, xdebut, ydebut, xfin, yfin)){
-            printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
-            scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+            printf("Entrez les coordonnées de la pièce que vous voulez déplacer, puis celles de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+            scanf("%d %d %d %d",&xd, &yd, &xf, &yf);
+            xdebut=convert_x(xd);
+            ydebut=convert_y(yd);
+            xfin=convert_x(xf);
+            yfin=convert_y(yf);
         }
         //fin du décompte pour le joueur noir
         affiche_plateau(plateau);
