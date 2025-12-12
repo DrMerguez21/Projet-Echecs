a466034a94b3aec2e77174098db653f4f92880f7
DrMerguez21 <pltguest@ad.polytech.sorbonne-universite.fr@pc4006-linux.ad.polytech.sorbonne-universite.fr>
2025-12-12 17:36:01 +0100

    Ajout de la fonction d'échecs

--- fini.c
+++ fini.c
@@ -11,4 +11,17 @@ int fini (Joueur* J1, Joueur* J2) {
     return (0) ;
 }
 
+int check (Piece*** plateau, int xroi, int yroi) {
+    int echec = 0 ;
+    for (int i = 0 ; i < 8 ; i++) {
+        for (int j = 0 ; j < 8 ; j++) {
+            if ((plateau[i][j] != NULL) && (plateau[i][j] -> C != plateau[xroi][yroi] -> C)) {
+                if (mouvement(plateau, i, j, xroi, yroi) == 1) {
+                    echec = 1 ;
+                    printf ("Echec de la pièce en %d %d", i, j) ;
+                }
+            }
+        }
+    } return echec ;
+}
 
