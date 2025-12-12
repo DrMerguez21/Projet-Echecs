b89a451ddb526f3e90acc784cfeb0b406111d56e
SoulK3eper <Youkie5222@free.fr>
2025-12-12 15:02:47 +0100

    création d'une partie simple, sans prise en compte des timeurs ou de la sauvegarde

---
+++ jeu.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "controle_plateau.h"
+//#include "mouv.h"
+
+int main(){
+    //ajouter ici la recharge de sauvegarde
+    Piece*** plateau=ini_plateau();
+    printf("les pièces noires sont écrites en minuscules, les blanches en majuscules, les x correspondent aux lignes et les y aux colonnes\n");
+
+    while(!fini(Piece*** plateau)){ //fonction fini pas encore faite, renvoie 1 si un roi a été dévoré
+        int xdebut, ydebut, xfin, yfin;
+        affiche_plateau(plateau);
+        printf("Au tour des blancs\n");
+        //debut du décompte pour le joueur blanc
+        printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+        scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+        while(!mouvement(plateau, xdebut, ydebut, xfin, yfin)){
+            printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+            scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+        }
+        //fin du décompte pour le joueur blanc
+        affiche_plateau(plateau);
+        printf("Au tour des noirs\n");
+        //debut du décompte pour le joueur noir
+        printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+        scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+        while(!mouvement(plateau, xdebut, ydebut, xfin, yfin)){
+            printf("Entrez le x et y de la pièce que vous voulez déplacer, puis ceux de la case où vous voulez aller, séparé à chaque fois par un espace\n");
+            scanf("%d %d %d %d",&xdebut, &ydebut, &xfin, &yfin);
+        }
+        //fin du décompte pour le joueur noir
+        affiche_plateau(plateau);
+    }
+}
