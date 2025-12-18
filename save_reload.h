d4174d1ab94aace38b4bec3b0b6ab38869f39e92
SoulK3eper <Youkie5222@free.fr>
2025-12-18 19:39:19 +0100

    je retire les fichiers save qui ont l'ancien nom

--- save_reload.h
+++
@@ -1,8 +0,0 @@
-#ifndef SAVE_RELOAD_H
-#define SAVE_RELOAD_H
-
-void save(const char* nom_fichier, Piece*** plateau, Joueur* J1, Joueur* J2, Couleur actuel);
-
-void reload(const char* nom_fichier, Piece*** plateau, Joueur* J1, Joueur* J2, Couleur* actuel);
-
-#endif
