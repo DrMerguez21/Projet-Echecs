c31371d67a36f845b959e9da42a1cca6fe096e5e
SoulK3eper <Youkie5222@free.fr>
2025-12-18 18:33:19 +0100

    ajout du rechargement dans le main, pas encore testé

--- jeu.c
+++ jeu.c
@@ -9,9 +9,28 @@
 
 int main(){
     //ajouter ici la recharge de sauvegarde
+    char reload;
+    printf("Voulez-vous recharger une partie? o ou n\n");
+    scanf("%c",&reload);
+    if(reload=='n'){
     Piece*** plateau=ini_plateau();
     Joueur* J1 = init_joueur (Blanc) ;
     Joueur* J2 = init_joueur (Noir) ;
     
     partie_blanc(plateau, J1, J2);
+    }
+    else{
+        Piece*** plateau=ini_plateau_vide();
+        Joueur* J1=malloc(sizeof(Joueur));
+        Joueur* J2=malloc(sizeof(Joueur));
+        Couleur actuel;
+        char nom_fichier[50];
+        printf("Entrez le nom de votre sauvegarde, en ajoutant bien .txt à la fin\n");
+        scanf("%49s", nom_fichier);
+        reload(nom_fichier, plateau, J1, J2, &actuel);
+        if(actuel== Blanc) partie_blanc(plateau, J1, J2);
+        else{
+            partie_noir(plateau, J1, J2);
+        }
+    }
 }
