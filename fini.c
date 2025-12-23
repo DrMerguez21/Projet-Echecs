1c2d080faa973cb91e3e4c0c0d06322724572c22
SoulK3eper <Youkie5222@free.fr>
2025-12-23 13:26:06 +0100

    modification des printf de check pour dire les couleurs affichée, pour faciliter la compréhension

--- fini.c
+++ fini.c
@@ -37,9 +37,9 @@ int check (Piece*** plateau, int xroi, int yroi) {
     for (int i = 0 ; i < 8 ; i++) {
         for (int j = 0 ; j < 8 ; j++) {
             if ((plateau[i][j] != NULL) && (plateau[i][j] -> C != plateau[xroi][yroi] -> C) && (mouvement(0,plateau, i, j, xroi, yroi, plateau[i][j] -> C) == 1)) {
-                if(plateau[xroi][yroi]==Blanc) printf ("Echec du roi blanc en %d %d", xroi, yroi) ;
+                if(plateau[xroi][yroi]==Blanc) printf ("Echec du roi bleu en %d %d", xroi, yroi) ;
                 else{
-                    printf ("Echec du roi noir en %c%d\n", reconvert_y(yroi), reconvert_x(xroi)) ;        
+                    printf ("Echec du roi rouge en %c%d\n", reconvert_y(yroi), reconvert_x(xroi)) ;        
                 }
                 return 1;
             }
